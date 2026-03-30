f044247c8b0d81eca740432f7cf0e32b3b74c7d6
Tristan <tristanpv11@gmail.com>
2026-03-30 21:00:18 +0200

    cm

--- index.js
+++ index.js
@@ -85,6 +85,13 @@ function mostrarSeccion(seccion){
         </div>
         `;
     }
+
+    if(seccion === "ubicacion"){
+        contenido.innerHTML = `<h2>Ubicación</h2>
+        <div class="ubicacion">
+        <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d464.23849067849403!2d-0.7774816203496921!3d38.487368246593235!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0xd63dacc1e0f63bf%3A0x1ec272fa486c487d!2sPiscina%20cubierta%20de%20San%20Fernando!5e0!3m2!1ses!2ses!4v1774895710142!5m2!1ses!2ses" width="800" height="450" style="border:0; border-radius: 15px;" allowfullscreen="" loading="lazy" referrerpolicy="no-referrer-when-downgrade"></iframe>
+        </div> `;
+    }
 }
 
     function cerrarImagen(){
