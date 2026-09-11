57c11124f5b313ab162b257177003d93917c1bd6
Tristán Pérez <tristanpv11@gmail.com>
2026-09-11 12:38:45 +0200

    Separa los horarios en un archivo de datos

---
+++ datos/horarios.js
@@ -0,0 +1,44 @@
+/*
+ * Horarios de entrenamiento del Club Waterpolo Petrer.
+ *
+ * Para modificar un horario, cambia únicamente el día, el grupo o la hora.
+ */
+
+const horariosEntrenamiento = [
+    {
+        dia: "Lunes",
+        sesiones: [
+            { grupo: "Alevín / Infantil", hora: "19:30 - 21:00" },
+            { grupo: "Juvenil / Absoluto Masculino / Absoluto Femenino", hora: "21:30 - 23:30" }
+        ]
+    },
+    {
+        dia: "Martes",
+        sesiones: [
+            { grupo: "Pre-Benjamín / Benjamín / Alevín", hora: "19:00 - 20:00" },
+            { grupo: "Alevín / Infantil", hora: "20:00 - 21:30" }
+        ]
+    },
+    {
+        dia: "Miércoles",
+        sesiones: [
+            { grupo: "Alevín / Infantil", hora: "19:00 - 20:30" },
+            { grupo: "Juvenil / Absoluto Masculino / Absoluto Femenino", hora: "21:30 - 23:30" }
+        ]
+    },
+    {
+        dia: "Jueves",
+        sesiones: [
+            { grupo: "Pre-Benjamín / Benjamín", hora: "19:00 - 20:00" },
+            { grupo: "Alevín / Infantil", hora: "19:00 - 20:30" },
+            { grupo: "Juvenil / Absoluto Masculino / Absoluto Femenino", hora: "21:30 - 23:30" }
+        ]
+    },
+    {
+        dia: "Viernes",
+        sesiones: [
+            { grupo: "Pre-Benjamín / Benjamín / Alevín / Infantil", hora: "19:30 - 21:00" },
+            { grupo: "Juvenil / Absoluto Masculino / Absoluto Femenino", hora: "20:30 - 22:30" }
+        ]
+    }
+];
