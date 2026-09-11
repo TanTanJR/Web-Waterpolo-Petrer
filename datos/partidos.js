7688eb32700909a5a62c0402c52cb18ca58970ec
Tristan <tristanpv11@gmail.com>
2026-09-11 12:55:28 +0200

    web casi completa

--- datos/partidos.js
+++ datos/partidos.js
@@ -39,5 +39,18 @@ const partidosCalendario = [ // Aquí se pueden agregar más partidos con su fec
             { hora: "16:00", categoria: "Alevín B", rival: "Rari Nantes" },
             { hora: "16:45", categoria: "Abs Masculino B", rival: "Rari Nantes" }
         ]
-    }
+    },
+    {
+        fecha: "2026-10-03",
+        ubicacion: "Piscina San Fernando, Petrer",
+        partidos: [
+            { hora: "09:00", categoria: "Alevín A", rival: "CN Godella" },
+            { hora: "09:45", categoria: "Infantil", rival: "CN Godella" },
+            { hora: "10:45", categoria: "Juvenil", rival: "CN Godella" },  
+            { hora: "11:45", categoria: "Abs Femenino", rival: "CN Godella" },
+            { hora: "12:45", categoria: "Abs Masculino A", rival: "CN Godella" }
+        ]
+    },
+
 ];
+
