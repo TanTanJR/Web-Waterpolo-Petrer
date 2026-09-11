b9e2d2802471b28c3933de778ff8fe204d216c4b
Tristán Pérez <tristanpv11@gmail.com>
2026-09-11 09:42:47 +0200

    Carga los partidos desde un archivo de datos

--- index.js
+++ index.js
@@ -7,40 +7,6 @@ let imagenProductoActual = 0;
 let listaProductos = [];
 let tipoLightbox = "galeria";
 
-const partidosCalendario = [ // Aquí se pueden agregar más partidos con su fecha, ubicación y detalles */
-    {
-        fecha: "2026-04-18",
-        ubicacion: "Piscina San Fernando, Petrer",
-        partidos: [
-            { hora: "14:00", categoria: "Alevín B", rival: "CW Turia" },
-            { hora: "14:00", categoria: "Alevín A", rival: "CW Turia" },
-            { hora: "14:45", categoria: "Infantil", rival: "CW Turia" },
-            { hora: "15:45", categoria: "Juvenil", rival: "CW Turia" },
-            { hora: "16:45", categoria: "Infantil", rival: "CW Turia" },
-            { hora: "17:45", categoria: "Abs Femenino", rival: "CW Turia" },
-            { hora: "18:45", categoria: "Abs Masculino A", rival: "CW Turia" }
-        ]
-    },
-    {
-        fecha: "2026-04-19",
-        ubicacion: "Piscina San Fernando, Petrer",
-        partidos: [
-            { hora: "09:00", categoria: "Alevín A", rival: "CN Godella" },
-            { hora: "09:45", categoria: "Infantil", rival: "CN Godella" },
-            { hora: "10:45", categoria: "Juvenil", rival: "CN Godella" },  
-            { hora: "11:45", categoria: "Abs Femenino", rival: "CN Godella" },
-            { hora: "12:45", categoria: "Abs Masculino A", rival: "CN Godella" }
-        ]
-    },
-    {
-       fecha: "2026-04-19",
-        ubicacion: "Valencia",
-        partidos: [
-            { hora: "16:00", categoria: "Alevín B", rival: "Rari Nantes" },
-            { hora: "16:45", categoria: "Abs Masculino B", rival: "Rari Nantes" }
-        ]
-    }
-];
 function mostrarSeccion(seccion){
     const contenido = document.getElementById("contenido"); // Aquí se muestra el contenido de cada sección según el botón pulsado */
     
